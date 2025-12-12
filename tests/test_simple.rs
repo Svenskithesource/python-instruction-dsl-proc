77567d67e75b25d1dba707498a5a3efa034903a4
svenskithesource <40274381+Svenskithesource@users.noreply.github.com>
2025-12-12 01:19:51 +0100

    Add syn's full features

--- tests/test_simple.rs
+++ tests/test_simple.rs
@@ -1,6 +1,6 @@
 use python_instruction_dsl_proc::define_opcodes;
 
 define_opcodes!(
-    NOP = 0 (value, unused[oparg] -- array[oparg]),
+    NOP = 0 (value, unused[oparg] -- array[if oparg {1} else {0}]),
     SPECIALIZED = 1 ( / ),
 );
