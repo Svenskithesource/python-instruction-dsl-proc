c3809217bceaf99861c415920df403eae12e3da9
svenskithesource <40274381+Svenskithesource@users.noreply.github.com>
2026-03-12 22:53:53 +0100

    Add associated type

--- src/lib.rs
+++ src/lib.rs
@@ -688,6 +688,7 @@ pub fn define_opcodes(input: TokenStream) -> TokenStream {
 
             impl GenericSIRNode for SIRNode {
                 type Opcode = Opcode;
+                type SIRException = SIRException;
 
                 fn new(opcode: Self::Opcode, oparg: u32, jump: bool) -> Self {
                     SIRNode::new(opcode, oparg, jump)
